05f1e8bb73eb4c95668cf2b6496d41f92497935c
Simona Miller <smiller@llcitadmins-MacBook-Pro-2.local>
2023-09-15 11:53:38 -0400

    update zenodo link

--- data/download_data_from_zenodo.sh
+++ data/download_data_from_zenodo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download and unzip 
-curl https://zenodo.org/record/8349403/files/gw190521_TD_PE.zip --output "gw190521_TD_PE.zip"
+curl https://zenodo.org/record/8349582/files/gw190521_TD_PE.zip --output "gw190521_TD_PE.zip"
 unzip gw190521_TD_PE.zip
 
 # Remove zip and Mac OSX files
